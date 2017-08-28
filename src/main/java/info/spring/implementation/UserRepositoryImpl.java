9406b46cdc7e17e2d90283248a7c548c96d218cc
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-08-28 09:02:57 +0200

    Commit

--- src/main/java/info/spring/implementation/UserRepositoryImpl.java
+++ src/main/java/info/spring/implementation/UserRepositoryImpl.java
@@ -7,7 +7,11 @@ import info.spring.domain.User;
 public class UserRepositoryImpl implements UsersRepository{
 	
 	private Logger logger;
-
+	
+	public UserRepositoryImpl(String dane){
+		System.out.println("dane: " + dane);
+	}
+	
 	public User createUser(String name) {
 		logger.log("tworzenie u¿ytknika " + name);
 		return new User(name);
